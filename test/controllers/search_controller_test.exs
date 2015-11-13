f761bde1e7326c8df87f9844aa0dc5ea5bc4ca2d
Eduard Litau <eduard.litau@gmail.com>
2015-11-13 21:27:58 +0100

    content for controller test

--- test/controllers/search_controller_test.exs
+++ test/controllers/search_controller_test.exs
@@ -2,7 +2,8 @@ defmodule PageChangeNotifier.SearchControllerTest do
   use PageChangeNotifier.ConnCase
 
   test "GET /search" do
-    # conn = get conn(), "/search"
-    # assert html_response(conn, 200) =~ "Search Results"
+    conn = get conn(), "/search", q: "query string"
+    assert html_response(conn, 200) =~ "Search Results"
+    assert html_response(conn, 200) =~ "query string"
   end
 end
