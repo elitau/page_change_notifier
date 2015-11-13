087b84762d8e83bad7d6a12ba50b12d565425530
Eduard Litau <eduard.litau@gmail.com>
2015-11-13 19:47:14 +0100

    Added heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :page_change_notifier, PageChangeNotifier.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "page-change-notifier.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
+config :page_change_notifier, PageChangeNotifier.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -56,6 +63,5 @@ config :logger, level: :info
 #     config :page_change_notifier, PageChangeNotifier.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Secrets are set in ENV
+#
