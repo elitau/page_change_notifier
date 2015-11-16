f6b99fd404cad9cb7cb17129b4d1228d09dc4bd6
Eduard Litau <eduard.litau@gmail.com>
2015-11-16 10:35:01 +0100

    Removed old notify test method call

--- lib/mix/tasks/page_change_notifier.notify.ex
+++ lib/mix/tasks/page_change_notifier.notify.ex
@@ -6,7 +6,6 @@ defmodule Mix.Tasks.PageChangeNotifier.Notify do
 
   def run(_args) do
     {:ok, _} = ensure_started(PageChangeNotifier.Repo)
-    new_results = PageChangeNotifier.NotifierJob.run("http://www.ebay-kleinanzeigen.de/s-50937/fahrrad/k0l18675r5")
     new_results = PageChangeNotifier.NotifierJob.run
     Mix.shell.info "Found #{length(new_results)} new results"
   end
