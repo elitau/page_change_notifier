0c1f0d412cdcf839da156cadf0c6f6b8cf213fce
Eduard Litau <eduard.litau@gmail.com>
2015-11-28 01:58:51 +0100

    no notification on empty results

--- web/models/notifier.ex
+++ web/models/notifier.ex
@@ -1,14 +1,18 @@
 defmodule PageChangeNotifier.Notifier do
-  def notify(search_agent, results) do
-    send_mail(search_agent.user.email, results)
+  def notify(_, []) do
+    # do nothing
+  end
+
+  def notify(search_agent, new_results) do
+    send_mail(search_agent.user.email, new_results)
     send_yo(search_agent.user.yo_username, search_agent.url)
   end
 
-  def send_mail(email, results) do
-    PageChangeNotifier.Mailer.send_new_results_text_email(email, results)
+  def send_mail(email, new_results) do
+    PageChangeNotifier.Mailer.send_new_results_text_email(email, new_results)
   end
 
-  def send_yo(nil, url) do
+  def send_yo(nil, _) do
     # do nothing
   end
 
