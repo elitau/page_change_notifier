f4c2961c8acbaac5fafd943319156cf9935027cb
Eduard Litau <eduard.litau@gmail.com>
2016-05-13 04:33:43 +0200

    Respond with chat_id only when asked for

--- web/controllers/telegram_controller.ex
+++ web/controllers/telegram_controller.ex
@@ -7,10 +7,8 @@ defmodule PageChangeNotifier.TelegramController do
       id   = params["message"]["chat"]["id"]
       text = params["message"]["text"]
       from = params["message"]["from"]["first_name"]
-      if text == "/id" do
-        Nadia.send_message(id, id)
-      else
-        Nadia.send_message(id, "Hello. Type /id to get the chat_id.")
+      if text == "/chat_id" do
+        Nadia.send_message(id, "Hello. Your chat id is #{id}")
       end
     end
     render conn, message: text
