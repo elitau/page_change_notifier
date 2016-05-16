65ff6fdacbc45106b2ea91f541f9c264f774ddd6
Eduard Litau <eduard.litau@gmail.com>
2016-05-16 21:49:22 +0200

    Allow longer urls in search agent

---
+++ priv/repo/migrations/20160516192543_make_url_longer.exs
@@ -0,0 +1,9 @@
+defmodule PageChangeNotifier.Repo.Migrations.MakeUrlLonger do
+  use Ecto.Migration
+
+  def change do
+    alter table(:search_agents) do
+      modify :url, :string, size: 2000
+    end
+  end
+end
--- test/models/search_agent_test.exs
+++ test/models/search_agent_test.exs
@@ -15,4 +15,14 @@ defmodule PageChangeNotifier.SearchAgentTest do
     changeset = SearchAgent.changeset(%SearchAgent{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "extra long url" do
+    long_url = "https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Sachsen/Leipzig/" <>
+               "62_1_145_8_10_23_138_146_31_32_143_35_37_57_38_40_47_48_49_52_60_13_64_67" <>
+               "_69_63_76_22_20_3_79_80/3,00-5,00/100,00-140,00/EURO-500,00-1300,00/1/128," <>
+               "117,118/-/true/-/-/-/-/-/-/-2011/true/-/-/-/1-?enteredFrom=result_list"
+
+    changeset = SearchAgent.changeset(%SearchAgent{}, %{url: long_url})
+    assert changeset.valid?
+  end
 end
