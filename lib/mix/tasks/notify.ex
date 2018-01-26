56256bbf39bf4ca6eb519f4ba4b97073ea346f2c
Eduard Litau <eduard.litau@gmail.com>
2018-01-26 22:33:34 +0100

    Readded notification task

---
+++ lib/mix/tasks/notify.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.PageChangeNotifier.Notify do
+  use Mix.Task
+
+  @shortdoc "Searches for new results and notifies user"
+
+  def run(_args) do
+    Mix.Task.run("app.start")
+
+    new_results = PageChangeNotifier.NotifierJob.run()
+    Mix.shell().info("Found #{length(new_results)} new results")
+  end
+end
